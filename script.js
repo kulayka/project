baa70ea64c1c6a4fbb95635a1c3a4232a487cefd
Ivan <is.kulaev@gmail.com>
2020-09-24 17:30:06 +0300

    add nOF

--- script.js
+++ script.js
@@ -6,7 +6,4 @@
 //const answer = prompt('Сколько вам лет?', '');
 //console.log(answer);
 
-const answers = [];
-answers[0] = prompt('Как ваше имя?', '');
-answers[1] = prompt('Как ваша фамилия?', '');
-answers[2] = prompt('Сколько вам лет?', '');
+const numberOfFilms = prompt('Сколько фильмов вы посмотрели?', '');

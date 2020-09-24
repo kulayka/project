8be23be2dd3cb52c520a9a748dd9a3836734b025
Ivan <is.kulaev@gmail.com>
2020-09-24 15:24:35 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+//console.log(1);
+
+//const result = confirm('Are you here?');
+//console.log(result);
+
+//const answer = prompt('Сколько вам лет?', '');
+//console.log(answer);
+
+const answers = [];
+answers[0] = prompt('Как ваше имя?', '');
+answers[1] = prompt('Как ваша фамилия?', '');
+answers[2] = prompt('Сколько вам лет?', '');
